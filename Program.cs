11c01852a1053f375351f96fc4a37705d1eead03
Jan Štefko <jan.stefko@email.cz>
2026-05-01 11:19:41 +0200

    Identity, DTOs, JWT,ApplicationUser

--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 using TODOApp.Data;
+using TODOApp.Models;
 using TODOApp.Repositories;
 using TODOApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Enable FrontEnd
-
+// CORS
 var AllowFrontend = "_allowFrontend";
-
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: AllowFrontend,
@@ -20,25 +24,92 @@ builder.Services.AddCors(options =>
         });
 });
 
-// Add services to the container.
-
+// Controllers
 builder.Services.AddControllers();
 
+// Database
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 6;
+
+    options.User.RequireUniqueEmail = true;
+})
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
+// JWT Authentication
+var jwtKey = builder.Configuration["Jwt:Key"]
+    ?? throw new InvalidOperationException("JWT Key není nakonfigurován.");
+var jwtIssuer = builder.Configuration["Jwt:Issuer"];
+var jwtAudience = builder.Configuration["Jwt:Audience"];
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtIssuer,
+        ValidAudience = jwtAudience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+    };
+});
+
+// Repositories a Services
 builder.Services.AddScoped<ITodoRepository, TodoRepository>();
-
 builder.Services.AddScoped<ITodoService, TodoService>();
+builder.Services.AddScoped<IJwtService, JwtService>();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
+// Swagger s podporou JWT
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "TODO App API", Version = "v1" });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Vlož JWT token. Příklad: \"eyJhbGc...\""
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
-// Automatically apply migrations on application startup
+// Automaticky aplikuj migrace při startu
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
@@ -46,7 +117,7 @@ using (var scope = app.Services.CreateScope())
     DbInitializer.Seed(db);
 }
 
-// Configure the HTTP request pipeline.
+// HTTP pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -54,9 +125,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseCors(AllowFrontend);
 
+app.UseAuthentication();   // ← NOVÉ! Musí být PŘED UseAuthorization
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ Services/IJwtService.cs
@@ -0,0 +1,10 @@
+﻿using TODOApp.Models;
+
+namespace TODOApp.Services
+{
+    public interface IJwtService
+    {
+        string GenerateToken(ApplicationUser user);
+        DateTime GetTokenExpiration();
+    }
+}
